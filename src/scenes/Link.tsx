fadaeb9c942b566a1d77b29321a007847040d84a
rzayev558 <rzayev558@gmail.com>
2023-10-18 19:49:43 +0200

    initial commit

---
+++ src/scenes/Link.tsx
@@ -0,0 +1,25 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "../shared/types";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      href={`#${lowerCasePage}`}
+      onClick={() => {
+        setSelectedPage(lowerCasePage);
+      }}
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-100 hover:text-primary-300`}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
